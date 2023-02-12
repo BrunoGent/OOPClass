5ede5e6e46f5ec6b1177219fa0003bf52993055a
Bruno Gentiletti <brunogentiletti@gmail.com>
2023-02-12 13:57:08 +0000

    commited

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+// Create a Main class
+
+// Create a class attribute integer called x
+
+// Create a class constructor for the Main class
+
+// Set the initial value for the class attribute x as 5
+
+//Add a main method
+// Create an object of class Main (This will call the constructor) called myObj
+
+// Print the value of x by calling t method myObj
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 5;
+        myObj(x);
+    }
+
+    static void myObj(int x) {
+        System.out.println(x);
+    }
+}
